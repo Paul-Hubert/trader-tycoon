d67c31ea0b45b731111e1fd0c84659b0e00ff320
Paul Boursin <paul.boursin@gmail.com>
2022-04-15 16:38:26 +0200

    added money

---
+++ PC3R_Projet/src/data/Money.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.text.DecimalFormat;
+
+public class Money {
+
+	private static DecimalFormat df = new DecimalFormat("#.##");
+	
+	public static String format(long money) {
+		return df.format(money / 100.0) + "$";
+		//return (Math.round(money) / 100.) + "$";
+		//return String.format("%. 2f", money / 100.0) + "$";
+	}
+	
+}
